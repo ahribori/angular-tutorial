201a961a11f447bb9925a20fce20eaad9b5b5467
daniel.hs <daniel.hs@kakaocorp.com>
2019-06-25 09:56:11 +0900

    hero service와 rx

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -11,9 +12,14 @@ export class HeroesComponent implements OnInit {
   selectedHero: Hero;
   heroes = HEROES;
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+  }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
   onSelect(hero: Hero): void {
