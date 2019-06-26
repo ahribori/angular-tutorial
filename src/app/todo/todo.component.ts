cf2a08748d1760ac038b2cfefb55ac74f642eeaa
ahribori <ahribori@gmail.com>
2019-06-27 00:17:30 +0900

    Todo 만들기

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Todo {
+  label: string;
+  done: boolean;
+}
+
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.scss']
+  styleUrls: ['./todo.component.scss'],
 })
 export class TodoComponent implements OnInit {
+  todoText = '';
+  list: Todo[] = [];
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
+  add(todo: Todo) {
+    this.list.push(todo);
+    this.list = Object.assign([], this.list);
+    this.todoText = '';
+  }
+
+  remove(item) {
+    this.list.splice(this.list.indexOf(item), 1);
+  }
+
+  handleKeyPress(e) {
+    if (e.key === 'Enter') {
+      this.add({ label: this.todoText, done: false });
+    }
+  }
 }
